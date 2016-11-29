750c738e2c8626ece2f60484c0fd2e6522a86a21
daniloBlera <danilo.cblera@gmail.com>
2016-11-29 18:57:46 -0300

    :D

--- server_front.py
+++ server_front.py
@@ -26,6 +26,24 @@ class Query1(Resource):
         connection.close()
         return result.__str__()
 
+class Record(Resource):
+    def __init__(self):
+        """Construtor."""
+        super(Record, self).__init__()
+
+    def get(self, month, year):
+        connection = db.connect()
+        item = connection.execute(
+                "select * from `RsiPsd`.`top3Posts` where `timeChanged` like '{0}-{1}-%%'".format(year, month))
+
+        # result = item.fetchone()
+        result = ''
+        for i in item:
+
+            result += str(i)+'\n'
+
+        connection.close()
+        return result
 
 
 app = Flask(__name__)
@@ -33,10 +51,10 @@ api = Api(app)
 auth = HTTPBasicAuth()
 
 api.add_resource(Query1, '/query1/', endpoint='query1')
+api.add_resource(Record, '/record/<string:month>/<string:year>/', endpoint='record')
 
 
 if __name__ == "__main__":
     """Inicia o servidor."""
     host = "0.0.0.0"
     app.run(host, debug=True)
-
